285b65657ea500fbd8b95d12dbd64cf4b421c10f
zechmeister <flo@mzechmeister.de>
2025-09-30 18:43:56 +0200

    feat: Initialize next app

---
+++ prettier.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "prettier";
+
+const config: Config = {
+  semi: true,
+  trailingComma: "es5",
+  singleQuote: false,
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+};
+
+export default config;
